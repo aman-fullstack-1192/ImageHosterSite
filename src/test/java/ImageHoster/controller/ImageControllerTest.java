8ebe92eddd4fe0eaee35f7431aa561ed0b3626ce
Aman kumar <aman@Amans-MacBook-Air.local>
2019-04-07 12:42:50 +0530

    minor modifications to pass all mockito test cases

--- src/test/java/ImageHoster/controller/ImageControllerTest.java
+++ src/test/java/ImageHoster/controller/ImageControllerTest.java
@@ -1,10 +1,11 @@
-/*
+
 package ImageHoster.controller;
 
 import ImageHoster.model.Image;
 import ImageHoster.model.Tag;
 import ImageHoster.model.User;
 import ImageHoster.model.UserProfile;
+import ImageHoster.service.CommentService;
 import ImageHoster.service.ImageService;
 import ImageHoster.service.TagService;
 import org.junit.Test;
@@ -39,6 +40,9 @@ public class ImageControllerTest {
 
     @MockBean
     private TagService tagService;
+
+    @MockBean
+    private CommentService commentService;
 
     //This test checks the controller logic to get all the images after the user is logged in the application and checks whether the logic returns the html file 'images.html'
     @Test
@@ -225,6 +229,14 @@ public class ImageControllerTest {
         image.setDescription("This image is for testing purpose");
         image.setUser(user1);
 
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dog");
+
+        List<Tag> tags = new ArrayList<>();
+        tags.add(tag);
+        image.setTags(tags);
+
 
         Mockito.when(imageService.getImage(Mockito.anyInt())).thenReturn(image);
 
@@ -310,4 +322,4 @@ public class ImageControllerTest {
     }
 }
 
-*/
+
